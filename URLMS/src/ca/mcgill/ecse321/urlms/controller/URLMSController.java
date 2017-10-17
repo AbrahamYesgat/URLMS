0c84a8cbd6cbcc753364a31ec042714d87e53728
CPanaritis <christos.panaritis.mail.mcgill.ca>
2017-10-17 15:29:01 -0400

    created the controller and the login method

---
+++ URLMS/src/ca/mcgill/ecse321/urlms/controller/URLMSController.java
@@ -0,0 +1,33 @@
+package ca.mcgill.ecse321.urlms.controller;
+
+import java.util.ArrayList;
+
+import ca.mcgill.ecse321.urlms.model.*;
+
+public class URLMSController {
+
+	private URLMS urlms;
+	
+	public URLMSController(URLMS urlms) {
+		this.urlms = urlms;
+	}
+	public boolean login(String email, String password) {
+		ArrayList<Laboratory> labs = (ArrayList<Laboratory>)urlms.getLaboratory();
+		if(urlms.numberOfLaboratory() == 0) {
+			ArrayList<Director> dirs = (ArrayList<Director>)urlms.getDir();
+			for (Director dir : dirs) {
+				if(dir.getEmail() == email && dir.getPassword() == password)
+					return true;
+			}
+		}
+		else {
+			for (Laboratory lab : labs) {
+				for (Staff member : lab.getStaffs()) {
+					if(member.getEmail() == email && member.getPassword() == password)
+						return true;	
+				}
+			}
+		}
+		return false;
+	}
+}
