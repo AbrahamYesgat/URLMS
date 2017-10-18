8ca6aa35fc4ca353f5269277d95d1dd7f866397e
CPanaritis <christos.panaritis.mail.mcgill.ca>
2017-10-18 15:49:43 -0400

    incomplete android project

---
+++ AppURLMS/app/src/main/java/ca/mcgill/ecse321/appurlms/MainActivity.java
@@ -0,0 +1,85 @@
+package ca.mcgill.ecse321.appurlms;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import ca.mcgill.ecse321.urlms.controller.URLMSController;
+import ca.mcgill.ecse321.urlms.model.*;
+import ca.mcgill.ecse321.urlms.persistence.PersistenceXStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private URLMS urlms = null;
+    private String fileName;
+    String error = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        fileName = getFilesDir().getAbsolutePath() + "/data.xml";
+        urlms = PersistenceXStream.initializeURLMS(fileName);
+        refreshData();
+    }
+
+    private void refreshData() {
+        TextView tv1 = (TextView) findViewById(R.id.user_email);
+        TextView tv2 = (TextView) findViewById(R.id.user_password);
+        tv1.setText("");
+        tv2.setText("");
+    }
+
+    public void login(View v) {
+        TextView tv1 = (TextView) findViewById(R.id.user_email);
+        TextView tv2 = (TextView) findViewById(R.id.user_password);
+        URLMSController cont = new URLMSController(urlms);
+
+        boolean isValid = cont.login(tv1.getText().toString(), tv2.getText().toString());
+        if(!isValid) {
+            refreshData();
+        }
+        else{
+            
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
