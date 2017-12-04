beb8b2f3b901a2a7b0e8043aefa699a313c62fee
CPanaritis <christos.panaritis@mail.mcgill.ca>
2017-12-03 23:32:45 -0500

    create progress report complete

--- AppURLMS/app/src/main/java/ca/mcgill/ecse321/appurlms/LabPage.java
+++ AppURLMS/app/src/main/java/ca/mcgill/ecse321/appurlms/LabPage.java
@@ -87,15 +87,27 @@ public class LabPage extends AppCompatActivity {
     }
 
     public void createProgressReport(View view) {
-        Intent intent = new Intent(LabPage.this, CreateProgressReport.class);
-        startActivity(intent);
-        finish();
+        if(cont.getActiveLaboratory().getActive()) {
+            Intent intent = new Intent(LabPage.this, CreateProgressReport.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            TextView labMessage = (TextView) findViewById(R.id.lab_message);
+            labMessage.setText("Cannot do this because the lab is inactive.");
+        }
     }
 
     public void viewProgressReport(View view) {
-        Intent intent = new Intent(LabPage.this, ViewProgressReports.class);
-        startActivity(intent);
-        finish();
+        if(cont.getActiveLaboratory().getActive()) {
+            Intent intent = new Intent(LabPage.this, ViewProgressReports.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            TextView labMessage = (TextView) findViewById(R.id.lab_message);
+            labMessage.setText("Cannot do this because the lab is inactive.");
+        }
     }
 
     public void deleteLab(View view) {
