08231298cc77703ed03e9a16923e38e9b04ce56a
CPanaritis <christos.panaritis@mail.mcgill.ca>
2017-12-05 02:23:11 -0500

    readdedweb

---
+++ WebURLMS/app/model/StaffRole.php
@@ -0,0 +1,14 @@
+<?php
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.26.1-f40f105-3613 modeling language!*/
+
+interface StaffRole
+{
+  // CONSTANT MEMBERS  
+
+ const ResearchAssociate = 0;
+ const ResearchAssistant = 1;
+
+  
+}
+?>
