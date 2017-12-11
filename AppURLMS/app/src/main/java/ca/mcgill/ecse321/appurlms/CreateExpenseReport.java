f384d79a247b38704ec35e1c0c60db757a4e7f19
CPanaritis <christos.panaritis@mail.mcgill.ca>
2017-12-11 16:27:50 -0500

    final code

--- AppURLMS/app/src/main/java/ca/mcgill/ecse321/appurlms/CreateExpenseReport.java
+++ AppURLMS/app/src/main/java/ca/mcgill/ecse321/appurlms/CreateExpenseReport.java
@@ -7,6 +7,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import static ca.mcgill.ecse321.appurlms.MainActivity.cont;
 
@@ -26,32 +27,29 @@ public class CreateExpenseReport extends AppCompatActivity {
         getSupportActionBar().setDisplayUseLogoEnabled(true);
     }
 
+    private String report;
+    private String value;
+
     /**
-     * Action listener for adding an expense report to the current lab.
-     * If the expense report is successfully added it returns the user to
-     * the view expense reports page.
-     * @see ViewExpenseReport
+     * Action listener for the add date button on the initial create expense report page.
+     * Once the user has entered all the fields on the first page, the next page displays in
+     * order for the user to select the date of the expense report.
      * @param view
      */
     public void addExpense(View view) {
         //Get all the fields.
         EditText expenseReport = (EditText) findViewById(R.id.expense_report);
         EditText amount = (EditText) findViewById(R.id.expense_amount);
-        DatePicker labStartDate = (DatePicker) findViewById(R.id.expense_date);
-        int day = labStartDate.getDayOfMonth();
-        int month = labStartDate.getMonth() + 1;
-        int year = labStartDate.getYear();
-        String report = expenseReport.getText().toString();
-        String value = amount.getText().toString();
+        TextView errorMessage = (TextView) findViewById(R.id.error_expense_message);
+        report = expenseReport.getText().toString();
+        value = amount.getText().toString();
 
         //Checks if the fields are empty.
         if(!(TextUtils.isEmpty(expenseReport.getText().toString().trim()) || TextUtils.isEmpty(amount.getText().toString().trim()))) {
-            boolean isValid = cont.createExpenseReport(report, Double.parseDouble(value), day, month, year);
-            if(isValid) {
-                Intent intent = new Intent(CreateExpenseReport.this, ViewExpenseReport.class);
-                startActivity(intent);
-                finish();
-            }
+            setContentView(R.layout.create_expense_select_date);
+        }
+        else{
+            errorMessage.setText("Missing info to create the expense report!");
         }
     }
 
@@ -77,6 +75,36 @@ public class CreateExpenseReport extends AppCompatActivity {
         boolean isValid = cont.logout();
         if(isValid) {
             Intent intent = new Intent(CreateExpenseReport.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    /**
+     * Action listener for the back button on the second page of the create expense report activity.
+     * Brings the user back to the first page of this activity.
+     * @param view
+     */
+    public void returnToPrevious(View view) {
+        setContentView(R.layout.create_expense_report);
+    }
+
+    /**
+     * Action listener for adding an expense report to the current lab.
+     * If the expense report is successfully added it returns the user to
+     * the view expense reports page.
+     * @see ViewExpenseReport
+     * @param view
+     */
+    public void createExpense(View view) {
+        DatePicker labStartDate = (DatePicker) findViewById(R.id.expense_date);
+        int day = labStartDate.getDayOfMonth();
+        int month = labStartDate.getMonth() + 1;
+        int year = labStartDate.getYear();
+
+        boolean isValid = cont.createExpenseReport(report, Double.parseDouble(value), day, month, year);
+        if(isValid) {
+            Intent intent = new Intent(CreateExpenseReport.this, ViewExpenseReport.class);
             startActivity(intent);
             finish();
         }
